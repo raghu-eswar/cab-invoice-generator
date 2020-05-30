a067e0605754545d7700456b2437345afaee3b6b
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-30 07:05:06 +0530

    Logic added to calculate fair.

--- src/main/java/com/cab/invoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/cab/invoiceGenerator/InvoiceGenerator.java
@@ -2,4 +2,10 @@ package com.cab.invoiceGenerator;
 
 public class InvoiceGenerator {
 
+    private static final double COST_PER_KM = 10.00;
+    private static final double COST_PER_MIN = 1.00;
+
+    public double calculateFair(double distance, int time) {
+        return (distance * COST_PER_KM)+(time * COST_PER_MIN);
+    }
 }
--- src/test/java/com/cab/invoiceGenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/cab/invoiceGenerator/InvoiceGeneratorTest.java
@@ -1,10 +1,21 @@
 package com.cab.invoiceGenerator;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+
+    public InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAAndTime_shouldReturnCorrectFair() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fair = invoiceGenerator.calculateFair(2.2, 3);
+        Assert.assertEquals(25, fair, 0.0);
     }
 }
